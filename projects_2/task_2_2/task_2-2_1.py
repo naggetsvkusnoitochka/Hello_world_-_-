b14dc512308a90e09ac5d9f789542b97c0cc87c1
Комракова Анастасия Станиславовна <nastya.stanisl@gmail.com>
2026-02-23 17:34:55 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,4 @@
+name_course = "Информатика"
+number_stage = 2
+quantity = 9
+print(f"Название курса: {name_course}, номер текущего этапа: {number_stage}, количество выполненных задач: {quantity}")
---
+++ projects_2/task_2_2/task_2-2_inventory_control.py
@@ -0,0 +1,4 @@
+
+name_reagent = input("Введите название реактива:")
+quantity = input("Введите количество реактива:")
+print(f"Реактив {name_reagent} поступил на склад в количестве {quantity} шт")
