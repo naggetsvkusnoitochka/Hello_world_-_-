4aec4571e40916e05821cdf0b9d99e16955948c1
Комракова Анастасия Станиславовна <nastya.stanisl@gmail.com>
2026-02-24 22:37:51 +0300

    Add files via upload

---
+++ projects_2/task_2_7/task_2-7_1.py
@@ -0,0 +1,4 @@
+files = ["seq1", "seq2", "seq3", "seq4"]
+for name in files:
+   new_name = name + ".18.12.2001.fasta"
+   print(f"{new_name}")
---
+++ projects_2/task_2_7/task_2-7_3.py
@@ -0,0 +1,6 @@
+seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
+for name in seq:
+    print(name)
+    for letter in name:
+       print(letter)
+print("Цикл выполнен")
